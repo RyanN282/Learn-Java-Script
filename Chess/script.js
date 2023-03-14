b8e94cebd799bb0b41f85b1909aa8f1e65679496
RyanNoscoe <99408961+RyanNoscoe@users.noreply.github.com>
2023-03-14 12:51:33 +0000

    Test

---
+++ Chess/script.js
@@ -0,0 +1,17 @@
+/*TO DO 
+-Create Board
+-Pawns
+-Knights
+-Bishops
+-Queen
+-Rooks
+-King
+-Capturing Pieces
+-Check
+-Checkmate
+-Stalemate
+-En Passante
+-Timers
+-Maybe Add an AI to play against
+*/
+
